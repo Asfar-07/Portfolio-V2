c4d365f0d5a9b0d12db347fd50eff7223b67e165
Asfar-07 <asfarbismi@gmail.com>
2026-03-23 23:50:27 +0530

    added a normal header

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,14 @@
 import "./globals.css";
+import Header from "@/components/layouts/Header";
 
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body >{children}</body>
+      <body>
+        <Header />
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div>
-      
-    </div>
+    <header className='fixed inset-0 flex justify-between items-center z-20 mx-auto w-[88%] max-w-350 min-w-100 h-22 text-(--p-font)' >
+      <div className=' uppercase font-bold text-3xl'>Logo</div>
+      <nav className=' flex list-none max-md:hidden'>
+        <ul className=' flex gap-15 font-bold'>
+          <li>HOME</li>
+          <li>ABOUT</li>
+          <li>PROJECT</li>
+          <li>CONTACT</li>
+        </ul>
+      </nav>
+    </header>
   )
 }
