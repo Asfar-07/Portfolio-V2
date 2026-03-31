675de2f0b89c23d87693326269dd01e1e74b2ae1
Asfar-07 <asfarbismi@gmail.com>
2026-03-31 12:41:16 +0530

    updated head

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,11 @@ import Header from "@/components/layouts/Header";
 import HeroBg from "@/components/layouts/HeroBg";
 import Footer from "@/components/layouts/Footer";
 
+export const metadata = {
+  title: "ASFAR MUHAMMED NS | Full Stack Developer",
+  description:
+    "Asfar is a software developer building modern, scalable web applications with clean design and high performance.",
+};
 
 export default function RootLayout({ children }) {
   return (
