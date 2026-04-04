e7de619752fd0f88d8e34a78427063c9212dab4b
Asfar-07 <asfarbismi@gmail.com>
2026-04-04 11:58:07 +0530

    Add react-use dependency and enhance Header component with scroll-based navigation

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,30 +1,61 @@
-import React from 'react'
+"use client"
+import React,{useEffect, useRef} from 'react'
 import "@/styles/header.css"
+import { useWindowScroll  } from 'react-use';
+import { gsap } from 'gsap';
 
 export default function Header() {
+  const [lastScrolled, setLastScrolled] = React.useState(0);
+  const [isNavVisible, setIsNavVisible] = React.useState(true);
+  const navContainer = useRef(null);
+  const navItems=["Home","About","Services","Contact"];
+  
+  const { y:currentScrollY } = useWindowScroll();
+
+  React.useEffect(() => {
+    console.log(currentScrollY);
+    if (currentScrollY === 0) {
+      setIsNavVisible(true);
+      navContainer.current.classList.remove("floating-nav")
+      navContainer.current.style.color="white"
+    }else if (currentScrollY > lastScrolled && currentScrollY > 100) {
+      setIsNavVisible(false);
+      navContainer.current.classList.add("floating-nav")
+       navContainer.current.style.color="black"
+    } else if(currentScrollY < lastScrolled) {
+      setIsNavVisible(true);
+      navContainer.current.classList.add("floating-nav")
+       navContainer.current.style.color="black"
+    }
+    setLastScrolled(currentScrollY);
+  }, [currentScrollY, lastScrolled]);  
+  React.useEffect(() => {
+    gsap.to(navContainer.current, {
+      y: isNavVisible ? 0 : -100,
+      opacity: isNavVisible ? 1 : 0,
+      duration: 0.5,
+    });
+  },[isNavVisible])
   return (
-    <header
-      id="header"
-      className="fixed flex  items-center w-full h-20 inset-0  z-20  text-(--p-font)"
-    >
-      <main className='m-auto w-[88%] max-w-350 min-w-100  flex items-center justify-between'>
-        <div className=" uppercase font-bold text-2xl">Asfar</div>
-        <nav className=" flex items-center list-none max-md:hidden">
-          <ul className=" flex items-center gap-10">
-            <a href="#home">HOME</a>
-            <a href="#about">ABOUT</a>
-            <a href="#project">PROJECT</a>
-            <a href="#contact">CONTACT</a>
-            <a
-              href="/cv.pdf"
-              download
-              className=" uppercase btn border border(--cyan-mark) font-bold"
-            >
-              Download CV
+   <div ref={navContainer} id='#header' className='fixed inset-x-0 top-4 z-50 h-15 border-0 transition-all duration-700 sm:inset-x-6 rounded-lg'>
+    <header className=' absolute top-1/2 w-full -translate-y-1/2'>
+    <nav className='flex size-full items-center justify-between p-4'>
+      <div className='flex items-center gap-7'>
+           <h4 className=" uppercase font-bold text-2xl m-0">Asfar</h4>
+      </div>
+      <div className='flex h-full item-center'>
+        <div className='hidden md:block'>
+          {navItems.map((item,index)=>(
+            <a key={index} href={`#${item.toLowerCase()}`} className='nav-hover-btn'>
+              {item}
             </a>
-          </ul>
-        </nav>
-      </main>
+          ))}
+        </div>
+      </div>
+    </nav>
     </header>
+
+
+   </div>
   );
 }
