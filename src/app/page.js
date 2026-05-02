e0a2fc40ff19371d4fe89976f6dc70305e500fe6
Asfar-07 <asfarbismi@gmail.com>
2026-05-02 13:21:44 +0530

    another ui designing and develop starting

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,4 @@
 import Hero from "@/components/sections/Hero";
-import About from "@/components/sections/About";
-import Project from "@/components/sections/Project";
 import Contact from "@/components/sections/Contact";
 import Experience from "@/components/sections/Experience";
 
@@ -8,9 +6,7 @@ export default function Home() {
   return (
     <div >
       <Hero />
-      <About />
       <Experience />
-      <Project />
       <Contact />
     </div>
   );
