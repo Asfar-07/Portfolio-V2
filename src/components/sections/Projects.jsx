28a105cc653c27ec2fe427538545f21a5e78e9c6
Asfar-07 <asfarbismi@gmail.com>
2026-05-28 23:38:25 +0530

    make responsive to project section

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -27,41 +27,45 @@ export default function Projects() {
     });
 
     cards.forEach((card, index) => {
-    if (index !== 0) {
-      tl.fromTo(
-        card,
-        {
-          yPercent: 140,
-          scale: 1.25
-        },
-        {
-          yPercent: -50,
-          scale: 1,
-          duration: 1,
-        }
-      );
-    }
+      if (index !== 0) {
+        tl.fromTo(
+          card,
+          {
+            yPercent: 140,
+            scale: 1.25,
+          },
+          {
+            yPercent: -50,
+            scale: 1,
+            duration: 1,
+          },
+        );
+      }
 
-    // last card fullscreen
-    if (index === cards.length - 1) {
-      tl.to(card, {
-        width: "100%",
-        height: "100%",
-        borderRadius: 0,
-        duration: 1,
-      });
-    }
-  });
+      // last card fullscreen
+      if (index === cards.length - 1) {
+        tl.to(card, {
+          width: "100vw",
+          height: "100vh",
+
+          maxWidth: "none",
+          maxHeight: "none",
+
+          borderRadius: 0,
+          duration: 1,
+        });
+      }
+    });
   }, [sectionRef]);
 
   return (
     <div
       id="projects"
-      className="projects bg-(--p-bg-deep) mt-20 text-(--p-font) h-auto overflow-hidden relative  w-full "
+      className="projects bg-(--p-bg-deep) mt-20 text-(--p-font) h-auto overflow-hidden relative  w-full"
     >
       <main
         ref={sectionRef}
-        className=" flex relative z-3 w-100%  h-screen text-(--p-font) max-md:flex-col-reverse  max-md:w-full  max-md:h-auto"
+        className=" flex relative z-3 w-100%  h-screen text-(--p-font) "
       >
         {projects.map((project, index) => {
         
@@ -70,9 +74,10 @@ export default function Projects() {
         return (
           <div
             key={index}
-            className="project-card flex absolute w-280 left-1/2 top-1/2 -translate-y-1/2  -translate-x-1/2 inset-0 h-160 bg-[#755EF4] rounded-2xl overflow-hidden"
+            className="project-card flex absolute w-[95%] max-w-280 left-1/2 top-1/2 -translate-y-1/2  -translate-x-1/2 inset-0 h-[95%] min-h-140  max-h-160 max-md:max-h-full  bg-[#755EF4] rounded-2xl overflow-hidden
+            "
           >
-            <section className="flex flex-col justify-between flex-1 h-full bg-[#661DE7]">
+            <section className="flex flex-col justify-between flex-1 h-full bg-[#661DE7] max-md:hidden">
               <header className="flex flex-col">
                 <div className="w-full h-15 border-b-2 border-[#755EF4]"></div>
                 <ul className="flex flex-col items-center gap-5 pt-5 w-full">
@@ -93,8 +98,9 @@ export default function Projects() {
               </header>
             </section>
 
-            <section className="flex flex-14 p-10">
-              <div className="flex flex-col justify-center gap-4 flex-1">
+            <section className="flex flex-14 p-10 max-md:p-7 gap-10 max-md:gap-5 max-sm:w-full max-sm:p-4
+            [@media(max-width:768px)_and_(min-height:650px)]:flex-col">
+              <div className="flex flex-col justify-center gap-4 flex-1 max-sm:w-full [@media(max-height:700px)]:gap-3">
                 <div className=" uppercase flex items-center tracking-normal gap-2 text-xs text-[#661DE7] font-bold px-3 py-1   border border-[#ffffff47] w-fit rounded-lg">
                   <span>
                     <Sparkles />
@@ -107,16 +113,16 @@ export default function Projects() {
                 >
                   <TypeIcon className="size-full p-2.5" color="white" />
                 </div>
-                <h2 className="text-5xl font-bold">{project.name}</h2>
-                <span className="w-20 h-1.5 bg-[#661DE7] rounded-2xl"></span>
-                <p className="text-2xl font-medium max-w-95">
+                <h2 className="text-5xl font-bold max-sm:text-3xl">{project.name}</h2>
+                <span className="w-20 h-1.5 bg-[#661DE7] rounded-2xl [@media(max-height:700px)]:hidden"></span>
+                <p className="text-2xl font-medium max-w-95 max-sm:text-xl">
                   {project.point}
                 </p>
-                <p className="text-sm font-light text-[#ffffffdc]">
+                <p className="text-sm font-light text-[#ffffffdc] max-sm:w-full">
                   {project.description}
                 </p>
 
-                <section className="flex gap-5 w-full text-xs font-medium pr-3">
+                <section className="flex gap-5 w-full text-xs font-medium pr-3 max-sm:flex-wrap [@media(max-height:700px)]:gap-2">
                   {project.features.map((feature, index) => {
                     const FeatureIcon = feature.icon;
                     return (
@@ -144,9 +150,10 @@ export default function Projects() {
                   </a>
                 </section>
               </div>
-              <aside className=" relative flex items-center flex-1">
-                <div className=" relative w-full h-[75%]">
-                  <div className=" absolute left-0 top-0 w-[85%] h-[70%] max-w-100 max-h-80 bg-amber-200 rounded-2xl overflow-hidden border border-white">
+              <aside className=" relative flex items-center flex-1 [@media_screen_and_(max-width:768px)_and_(max-height:768px)]:hidden">
+                <div className=" relative w-full h-[75%] max-lg:h-[55%] [@media(max-width:768px)_and_(min-height:768px)]:h-full">
+                  <div className=" absolute left-0 top-0 w-[85%] h-[70%] max-w-100 max-h-80 bg-amber-200 rounded-2xl overflow-hidden border border-white
+                  max-lg:max-h-50 [@media(max-width:768px)_and_(min-height:768px)]:hidden">
                     <img
                       className="size-full "
                       loading="lazy"
@@ -154,7 +161,9 @@ export default function Projects() {
                       alt="prof"
                     />
                   </div>
-                  <div className=" absolute  right-0 bottom-0 w-[85%] h-[70%] max-w-100 max-h-80 bg-emerald-700 rounded-2xl overflow-hidden border border-white">
+                  <div className=" absolute  right-0 bottom-0 w-[85%] h-[70%] max-w-100 max-h-80 bg-emerald-700 rounded-2xl overflow-hidden border border-white
+                  max-lg:max-h-50 [@media(max-width:768px)_and_(min-height:768px)]:w-full [@media(max-width:768px)_and_(min-height:768px)]:max-w-none
+                  [@media(max-width:768px)_and_(min-height:768px)]:h-full [@media(max-width:768px)_and_(min-height:768px)]:max-h-none">
                      <img
                       className="size-full "
                       loading="lazy"
