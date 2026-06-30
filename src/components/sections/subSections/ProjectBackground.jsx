842cd0c5e5b69375bd68c9b8a76ef67d5c73b13a
Asfar-07 <asfarbismi@gmail.com>
2026-06-30 22:29:49 +0530

    update project ui

--- src/components/sections/subSections/ProjectBackground.jsx
+++ src/components/sections/subSections/ProjectBackground.jsx
@@ -33,10 +33,7 @@ export default function ProjectBackground({bgImage, mainImage}) {
     <section className="absolute min-h-[600px] inset-0 w-full h-screen overflow-hidden">
       <div ref={bgImage} className="masked-section w-full  h-[125%] ">
         <div className=" absolute inset-0 bg-[#663e97] z-10"></div>
-        <div
-          ref={containerRef}
-          className=" relative size-full  z-15"
-        >
+        <div ref={containerRef} className=" relative size-full  z-15">
           <div ref={circleRef} className="w-75 h-75 absolute top-0 left-0">
             <div className="mouse-child size-full rounded-full pointer-events-none bg-[#00eeff] blur-[14px]"></div>
             <div className=" absolute inset-0 rounded-full overflow-hidden">
@@ -71,6 +68,72 @@ export default function ProjectBackground({bgImage, mainImage}) {
           />
         </div>
         <div className=" absolute w-full h-[20%] left-0 bottom-0 z-25 max-sm:h-[16%]">
+          <div className=" absolute left-[34%] bottom-[45%] w-[6%] h-[25%] z-25">
+            {[...Array(10)].map((_, index) => (
+              <span
+                key={index}
+                className="dust-custom w-1 h-1 bg-[#ff00ea]"
+                style={{
+                  left: `${Math.random() * 100}%`,
+                  animationDelay: `${Math.random() * 5}s`,
+                  animationDuration: `${5 + Math.random() * 5}s`,
+                }}
+              />
+            ))}
+          </div>
+          <div className=" absolute left-[0%] bottom-[45%] w-[6%] h-[25%] z-25">
+            {[...Array(10)].map((_, index) => (
+              <span
+                key={index}
+                className="dust-custom w-1 h-1 bg-[#ff00ea]"
+                style={{
+                  left: `${Math.random() * 100}%`,
+                  animationDelay: `${Math.random() * 5}s`,
+                  animationDuration: `${5 + Math.random() * 5}s`,
+                }}
+              />
+            ))}
+          </div>
+          <div className=" absolute left-[95%] bottom-[45%] w-[6%] h-[25%] z-25">
+            {[...Array(10)].map((_, index) => (
+              <span
+                key={index}
+                className="dust-custom w-1 h-1 bg-[#ff00ea]"
+                style={{
+                  left: `${Math.random() * 100}%`,
+                  animationDelay: `${Math.random() * 5}s`,
+                  animationDuration: `${5 + Math.random() * 5}s`,
+                }}
+              />
+            ))}
+          </div>
+
+          <div className=" absolute left-[50%] bottom-[5%] w-[6%] h-[25%] z-25">
+            {[...Array(10)].map((_, index) => (
+              <span
+                key={index}
+                className="dust w-1 h-1 bg-[#ff00ea]"
+                style={{
+                  left: `${Math.random() * 100}%`,
+                  animationDelay: `${Math.random() * 5}s`,
+                  animationDuration: `${5 + Math.random() * 5}s`,
+                }}
+              />
+            ))}
+          </div>
+          <div className=" absolute left-[84%] bottom-[58%] w-[6%] h-[25%] z-25">
+            {[...Array(10)].map((_, index) => (
+              <span
+                key={index}
+                className="dust w-1 h-1 bg-[#ff00ea]"
+                style={{
+                  left: `${Math.random() * 100}%`,
+                  animationDelay: `${Math.random() * 5}s`,
+                  animationDuration: `${5 + Math.random() * 5}s`,
+                }}
+              />
+            ))}
+          </div>
           <Image
             src="/images/project-elements/backgroundBottom.webp"
             alt="project background"
