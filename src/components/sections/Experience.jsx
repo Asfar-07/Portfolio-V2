d933418f62dbb85a5a0c93a22cacec6fd2ac0116
Asfar-07 <asfarbismi@gmail.com>
2026-07-09 13:07:20 +0530

    Experience section to use dynamic data and improve layout with responsive skills display

--- src/components/sections/Experience.jsx
+++ src/components/sections/Experience.jsx
@@ -23,53 +23,38 @@ export default function Experience() {
          max-w-[1250px] min-h-[600px] max-md:w-full max-md:h-screen`}
        >
          <section className=" absolute inset-0 ">
-           <aside className={`experience-card absolute left-0 top-1/2 p-10 max-w-155 -translate-y-1/2 b rounded-2xl 
+          {experiences.map((exp,index)=>(
+           <aside key={index} className={`experience-card absolute ${index % 2 === 0 ? 'left-0' : 'right-0'} top-1/2 p-10 max-w-155 -translate-y-1/2 b rounded-2xl 
            border border-[#04deff78] bg-transparent backdrop-blur-3xl max-md:p-5`}>
              {/* <div className='flex items-center gap-2 text-[#cdbbfc] uppercase w-fit text-[12px] rounded-xl px-2.5 py-px bg-[#3e00c53b]'>
               <University className=" size-3" />
               Education
              </div> */}
              <h4 className=" font-bold text-4xl uppercase my-1">
-               BSc Computer Science
+               {exp.title}
              </h4>
              <div className="italic flex items-center mt-2 text-[15px]">
-               <div className='p-1.5 bg-[#3e00c57a] mr-2 rounded-full'><University className="text-[#9d77ff] size-5" /></div> University of
-               Kerala <span className="ml-4 text-[#9d77ff]">2021 - 2024</span>
+               <div className='p-1.5 bg-[#3e00c57a] mr-2 rounded-full'>{exp.company_name[1]}</div> 
+               {exp.company_name[0]} <span className="ml-4 text-[#9d77ff]">{exp.date}</span>
              </div>
              <div className="mt-5 text-[#ffffffba] font-extralight text-[15px] w-full max-w-130 leading-[1.5] tracking-wider">
-               Pursued a full undergraduate degree in computer science, gaining
-               deep expertise in data structures, algorithms, software
-               engineering, and more.
+               {exp.points}
              </div>
              <div className="flex flex-wrap gap-2  mt-4 text-[13px] tracking-wider font-light">
-               {/* <div class>Data Structures & Algorithms</div> */}
-               <span className="flex gap-1 px-3 py-2 border border-[#ffffff32] rounded-xl items-center">
-                  <div className='p-1.5 bg-[#5100ff7a] mr-2 rounded-lg'><University className="text-[#9d77ff] size-5.5" /> </div>
-                  Data Structures & Algorithms
-               </span>
-               <span className="flex gap-1 px-3 py-2 border border-[#ffffff32] rounded-xl items-center">
-                  <div className='p-1.5 bg-[#5100ff7a] mr-2 rounded-lg'><University className="text-[#9d77ff] size-5.5" /> </div>
-                 Software
-                 Engineering
-               </span>
-               <span className="flex gap-1 px-3 py-2 border border-[#ffffff32] rounded-xl items-center">
-                  <div className='p-1.5 bg-[#5100ff7a] mr-2 rounded-lg'><University className="text-[#9d77ff] size-5.5" /> </div>
-                  Database Systems
-               </span>
-               <span className="flex gap-1 px-3 py-2 border border-[#ffffff32] rounded-xl items-center">
-                 <div className='p-1.5 bg-[#5100ff7a] mr-2 rounded-lg'><University className="text-[#9d77ff] size-5.5" /></div> Operating
-                 Systems
-               </span>
-               {/* <span className="flex gap-1 px-3 py-2 border border-[#ffffff8c] rounded-xl items-center">
-                  <div className='p-1.5 bg-[#5100ff7a] mr-2 rounded-lg'><University className="text-[#9d77ff] size-5.5" /> </div>
-                 Networking
-               </span> */}
+              {exp.skills.map((skill,index)=>(
+                <span key={index} className="flex gap-1 px-3 py-2 border border-[#ffffff32] rounded-xl items-center">
+                  <div className='p-1.5 bg-[#5100ff7a] mr-2 rounded-lg'>{skill.icon}</div>
+                  {skill.name}
+                </span>
+              ))}
              </div>
              <button className="flex items-center gap-2 mt-8 px-5 py-2 text-[14px] bg-[#5917ff] tracking-wider rounded-3xl uppercase cursor-pointer tracking-wider">
                <FileBadge className=' size-5'/>
                Certification
              </button>
            </aside>
+          ))}
+           
          </section>
          <div ref={robotBody} className=" relative size-140">
            <div className=" absolute top-[25%] -left-[35%]  scale-y-[1.2] scale-x-[1.5] overflow-hidden">
--- src/store/db/experience.js
+++ src/store/db/experience.js
@@ -1,50 +1,146 @@
+import { University, Building2, Computer, Sigma, UtensilsCrossed, Grid2x2Plus,
+    Braces, Database, Globe, Waypoints, SquareStack, Code, MonitorCog, Handshake,
+    Cloud, FileStack, NotebookPen, Shield
+ } from "lucide-react";
 const experiences=[
-    {
-        title: "Higher Secondary - Computer Science",
-        company_name:"snghss chempazhanthy",
-        icon:"/images/EducationIcon.webp",
-        iconBg:"",
-        date: "2019 - 2021",
-        points: "Built the foundational knowledge in computer science, mathematics, and logical thinking that shaped every step ahead.",
-        skills: ["Computer Science",
-            "Mathematics",
-            "Logic & Problem Solving"]
-    },
+    // {
+    //     title: "Higher Secondary - Computer Science",
+    //     company_name:["snghss chempazhanthy", <University />],
+    //     icon:"/images/EducationIcon.webp",
+    //     iconBg:"",
+    //     date: "2019 - 2021",
+    //     points: "Built the foundational knowledge in computer science, mathematics, and logical thinking that shaped every step ahead.",
+    //     certificate:"",
+    //     skills: [{
+    //         name: "Computer Science",
+    //         icon: <Computer />
+    //     },
+    //     {
+    //         name: "Mathematics",
+    //         icon: <Sigma />
+    //     },
+    //     {
+    //         name: "Logic & Problem Solving",
+    //         icon: <UtensilsCrossed />
+    //     }
+    //     ]
+    // },
     {
         title: "BSc Computer Science",
-        company_name:"University of Kerala",
+        company_name:["University of Kerala", <University />],
         icon:"/images/EducationIcon.webp",
         iconBg:"",
         date: "2021 - 2024",
         points: "Pursued a full undergraduate degree in computer science, gaining deep expertise in data structures, algorithms, software engineering, and more.",
-        skills: ["Data Structures & Algorithms",
-            "Software Engineering",
-            "Database Systems",
-            "Operating Systems",
-            "Networking"]
+        certificate:"",
+        skills: [{
+            name: "Data Structures & Algorithms",
+            icon: <Grid2x2Plus />
+        },
+        {
+            name: "Software Engineering",
+            icon: <Braces />
+        },
+        {
+            name: "Database Systems",
+            icon: <Database />
+        },
+        {
+            name: "Operating Systems",
+            icon: <University />
+        },
+        {
+            name: "Networking",
+            icon: <Globe />
+        }]
     },
     {
         title: "Java Full Stack Development",
-        company_name:"University of Kerala",
+        company_name:["KIDE", <Building2  />],
         icon:"/images/KIDE.webp",
         iconBg:"",
-        date: "8 Month",
+        date: "2024 - 2025",
         points: "Completed an intensive full-stack program focused on building production-ready web applications from front to back.",
-        skills: ["Java",
-            "Spring Boot",
-            "REST APIs",
-            "HTML / CSS / JS",
-            "MySQL",
-            "Git"],
+        certificate:"",
+        skills: [{
+            name: "Java & Spring Boot",
+            icon: <Code />
+        },
+        {
+            name: "API Development",
+            icon: <Waypoints />
+        },
+        {
+            name: "MySQL",
+            icon: <Database />
+        },
+        {
+            name: "Version Control",
+            icon: <SquareStack />
+        },
+       {
+            name: "Logic & Problem Solving",
+            icon: <UtensilsCrossed />
+        }]
     },
      {
-        title: "Career Break",
-        company_name:"Self-Directed Learning",
+        title: "Internship",
+        company_name:["OXOPIX BUSINESS SOLUTIONS", <Building2 />],
         icon:"/images/EducationIcon.webp",
         iconBg:"",
-        date: "2025 - Present",
-        points: "Taking intentional time to explore modern software development beyond the classroom — experimenting with new tools, frameworks, and building personal projects.",
-        skills: ["Open source","Three.js","Next.js","Microservices"]
+        date: "2025 - 2026",
+        points: "Worked closely with senior developers to build multiple applications, gaining hands-on experience in scalable MVP development, microservices, clean architecture, and modern security practices within a six-month internship.",
+        certificate:"",
+        skills: [{
+            name: "System Design",
+            icon: <MonitorCog />
+        },
+        {
+            name: "Team Collaboration",
+            icon: <Handshake />
+        },
+        {
+            name: "Security",
+            icon: <Shield />
+        },
+        {
+            name: "MVP",
+            icon: <NotebookPen />
+        },
+        {
+            name: "Microservices",
+            icon: <FileStack />
+        },
+        {
+            name: "Cloud Deployment",
+            icon: <Cloud />
+        },
+       ]
     }
+    //  {
+    //     title: "Career Break",
+    //     company_name:["Self-Directed Learning", <University />],
+    //     icon:"/images/EducationIcon.webp",
+    //     iconBg:"",
+    //     date: "2025 - Present",
+    //     points: "Taking intentional time to explore modern software development beyond the classroom — experimenting with new tools, frameworks, and building personal projects.",
+    //     certificate:"",
+    //     skills: [{
+    //         name: "Open source",
+    //         icon: <University />
+    //     },
+    //     {
+    //         name: "Three.js",
+    //         icon: <University />
+    //     },
+    //     {
+    //         name: "Next.js",
+    //         icon: <University />
+    //     },
+    //     {
+    //         name: "Microservices",
+    //         icon: <University />
+    //     }]
+    // }
 ]
 export {experiences};
